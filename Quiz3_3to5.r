3e86ba549b3aca324289e22cf02120df4604158b
unknown <tcourtney@TCOURTNEYRMT7.rochester.inetgleason.com>
2014-05-15 18:31:35 +0200

    added edu code and data

--- Quiz3_3to5.r
+++ Quiz3_3to5.r
@@ -1,5 +1,6 @@
 # This is the R script for Gathering and Cleaning data - Project Assignement
 
+#Read in and clean up GDP data
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv ","FGDP.csv")
 gpd <- read.csv("FGDP.csv", stringsAsFactors=FALSE, header=FALSE)
 gpd<-gpd[6:220,]
@@ -8,3 +9,7 @@ gpd<-gpd[,1:5]
 gpd<-gpd[-3]
 gpd$GPD_Rank<-as.integer(gpd$GPD_Rank)
 
+#Read in and clean up educational data
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv ","FEDSTATS.csv")
+edu<-read.csv("FEDSTATS.csv",stringsAsFactors=FALSE, header=TRUE)
